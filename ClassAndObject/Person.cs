b7809a5c12381b009cc365cc728361ff19ce8d7e
unknown <jobayerdpi11@gmail.com>
2021-04-02 09:27:00 +0600

    class and object practice

---
+++ ClassAndObject/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    class Person //user define class not free define. 
+    {
+        public string firstName; //this are called field,propertys,instance variable etc.
+        public string middleName;
+        public string lastName;
+
+        public string getFullName()
+        {
+            string fullName = firstName + " " + middleName + " " + lastName;
+            return fullName;
+        }
+    }
+}
---
+++ ClassAndObject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person per = new Person(); //call by user
+            per.firstName = "Md.";
+            per.middleName = "Rofiq";
+            per.lastName = "Udding";
+            Person per1 = new Person(); //Here per1 is representing person class object. this is called instance
+            per1.firstName = "Mohammad";
+            per1.middleName = "Sharif";
+            per1.lastName = "Uddin";
+            Person per2 = new Person();
+            per2.firstName = "Shakib";
+            per2.middleName = "Al";
+            per2.lastName = "Hasan";
+            string fullname = per.getFullName();
+            string fullname1 = per1.getFullName();
+            string fullname2 = per2.getFullName();
+            Console.WriteLine("His name is : {0}",fullname);
+            Console.WriteLine("His name is : {0}",fullname1);
+            Console.WriteLine("His name is : {0}",fullname2);
+            Console.ReadKey();
+        }
+    }
+}
